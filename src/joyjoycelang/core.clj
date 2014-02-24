a0ffd49f7c44413bc98c2e88e7133cc69ea3e3ba
timsgardner <timsgardner@gmail.com>
2014-02-24 15:39:21 -0500

    First commit

---
+++ src/joyjoycelang/core.clj
@@ -0,0 +1,6 @@
+(ns joyjoycelang.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/joyjoycelang/core_test.clj
@@ -0,0 +1,7 @@
+(ns joyjoycelang.core-test
+  (:use clojure.test
+        joyjoycelang.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
