7a156c7b74af6255780576fe079dda2887b6c42a
timsgardner <timsgardner@gmail.com>
2014-02-24 20:24:04 -0500

    tests passing yall

--- src/joyjoycelang/kernel.clj
+++ src/joyjoycelang/kernel.clj
@@ -1,9 +1,15 @@
-(ns joyjoycelang.kernel)
+(ns joyjoycelang.kernel
+  (:use [clojure.test :only [is]]))
 
 (defn split-vec [v, i]
   (let [split-inx (if (pos? i), i, (+ (count v) i))]
     [(subvec v 0 split-inx)
      (subvec v split-inx)]))
+
+(defn remove-nil [x]
+  (if (vector? x)
+    (mapv remove-nil (remove nil? x)),
+    x))
 
 (declare run)
 
@@ -13,11 +19,12 @@
 (def joyenv
   (atom
    {:cake (fn cake [stack]
-            (let [[stack', [[b], [a]]] (split-vec stack -2)]
-              (conj stack', [[b] a], [a [b]])))
+            (let [[stack', [[&_ :as bv], [&_ :as av]]] (split-vec stack -2)]
+              ;; this is stupid because of destructuring crap. Means: [[b]a] [a[b]]
+              (run stack', (remove-nil [(into [bv] av), (conj av bv)]))))
     :k (fn k [stack]
-         (let [[stack', [[b], [a]] (split-vec stack -2)]]
-           (conj stack', a)))
+         (let [[stack', [[&_], [&_ :as av]]] (split-vec stack -2)]
+           (run stack', (remove-nil av))))
     :def (fn def-op [stack]
            (let [stack' (pop stack)
                  [val name] (peek stack)]
@@ -28,18 +35,35 @@
                                (run stack'' val)))))
              stack'))}))
 
-(defn step-dispatch [stack, head]
-  (if (find @joyenv head), :fn, :default))
-
-(def step nil)
-(defmulti step #'step-dispatch)
-
-(defmethod step :fn [stack, head]
-  ((@joyenv head) stack))
-
-(defmethod step :default [stack, head]
-  (conj stack head))
+(defn step [stack, head]
+  (cond
+   (nil? head) stack,
+   (contains? @joyenv head) ((@joyenv head) stack),
+   :else (conj stack head)))
 
 (defn run
   ([src] (run [] src))
   ([stack src] (reduce step stack src)))
+
+;; bootstrap --------------------------------------------
+
+(run
+  [[[[] :k] :zap] :def
+   [[:cake :k] :dip] :def
+   [[:cake [] :k] :cons] :def
+   [[[[]] :dip :k] :i] :def
+   [[[] :cake :dip :dip] :dup] :def])
+
+(is (= (run [[:b] [:a] :cake]) [[[:b] :a] [:a [:b]]]))
+
+(is (= (run [[:b] [:a] :k]) [:a]))
+
+(is (= (run [[:a] :zap]) []))
+
+(is (= (run [[:b] [:a] :dip]) [:a [:b]]))
+
+(is (= (run [[:b] [:a] :cons]) [[[:b] :a]]))
+
+(is (= (run [[:a] :i]) [:a]))
+
+(is (= (run [[:a] :dup]) [[:a] [:a]]))
